fa53094fc622cbd890bb3f4a994b96ed82ea830a
josegarcia19809 <josegarcia19809@gmail.com>
2024-06-30 20:48:33 -0600

    PROYECTO REACT TS-Contador de Calorías: Agregando el Formulario de Entrada de datos

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,50 @@
+import {categories} from "../data/categories.ts";
+
 function Form() {
     return (
-        <div>
-            <p>Formulario</p>
-        </div>
+        <form
+            className="space-y-5 bg-white shadow p-10 rounded-lg"
+        >
+            <div className="grid grid-cols-1 gap-3">
+                <label htmlFor="category" className="font-bold">Categoría: </label>
+                <select
+                    className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+                    id="category"
+                >
+                    {categories.map(category => (
+                        <option key={category.id} value={category.id}>
+                            {category.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
+            <div className="grid grid-cols-1 gap-3">
+                <label htmlFor="activity" className="font-bold">Actividad: </label>
+                <input
+                    id="activity"
+                    type="text"
+                    className="border border-slate-300 p-2 rounded-lg"
+                    placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio"
+                />
+            </div>
+
+            <div className="grid grid-cols-1 gap-3">
+                <label htmlFor="calories" className="font-bold">Calorías: </label>
+                <input
+                    id="calories"
+                    type="number"
+                    className="border border-slate-300 p-2 rounded-lg"
+                    placeholder="Ej. 300 o 500"
+                />
+            </div>
+            <input
+                type="submit"
+                className="bg-gray-800 hover:border-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
+                value="Guardar comida o guardar ejercicio"
+            />
+
+        </form>
     );
 }
 
