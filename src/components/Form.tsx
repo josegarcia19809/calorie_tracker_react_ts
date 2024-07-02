ba28f3dd8cf9a2573f0e1f286ac5d5f6c1aba7e5
josegarcia19809 <josegarcia19809@gmail.com>
2024-07-01 19:30:26 -0600

    PROYECTO REACT TS-Contador de Calorías: Creando el State para las Actividades

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,14 @@
 import {categories} from "../data/categories.ts";
+import {useState} from "react";
 
 function Form() {
+
+    const [activity, setActivity] = useState({
+        category: 1,
+        name: "",
+        calories: 0
+    });
+
     return (
         <form
             className="space-y-5 bg-white shadow p-10 rounded-lg"
@@ -10,6 +18,7 @@ function Form() {
                 <select
                     className="border border-slate-300 p-2 rounded-lg w-full bg-white"
                     id="category"
+                    value={activity.category}
                 >
                     {categories.map(category => (
                         <option key={category.id} value={category.id}>
@@ -20,12 +29,13 @@ function Form() {
             </div>
 
             <div className="grid grid-cols-1 gap-3">
-                <label htmlFor="activity" className="font-bold">Actividad: </label>
+                <label htmlFor="name" className="font-bold">Actividad: </label>
                 <input
-                    id="activity"
+                    id="name"
                     type="text"
                     className="border border-slate-300 p-2 rounded-lg"
                     placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio"
+                    value={activity.name}
                 />
             </div>
 
@@ -36,6 +46,7 @@ function Form() {
                     type="number"
                     className="border border-slate-300 p-2 rounded-lg"
                     placeholder="Ej. 300 o 500"
+                    value={activity.calories}
                 />
             </div>
             <input
