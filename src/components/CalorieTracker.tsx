28b986932e808f1c980a2a5c88ded6946c4f9bde
josegarcia19809 <josegarcia19809@gmail.com>
2024-07-17 07:23:37 -0600

    PROYECTO REACT TS-Contador de Calorías: Mostrando las calorias consumidas

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,12 +1,28 @@
 import {Activity} from "../types";
+import {useMemo} from "react";
 
-type CalorieTrackerProps={
+type CalorieTrackerProps = {
     activities: Activity[]
 }
 
 function CalorieTracker({activities}: CalorieTrackerProps) {
+
+    // contadores
+    const caloriesConsumed = useMemo(() => activities.reduce((total, activity) =>
+        activity.category === 1 ? total + activity.calories : total, 0), [activities])
     return (
-        <div>CalorieTracker</div>
+        <>
+            <h2 className="text-4xl font-black text-white text-center">Resumen de calorías</h2>
+
+            <div
+                className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
+                <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
+                    <span className="font-black text-6xl text-orange-400">
+                    {caloriesConsumed}
+                    </span> Consumidas
+                </p>
+            </div>
+        </>
     );
 }
 
