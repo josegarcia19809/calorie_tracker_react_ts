787765cdefde45ea1d7fa3eac079eed908faf115
josegarcia19809 <josegarcia19809@gmail.com>
2024-07-16 22:36:16 -0600

    PROYECTO REACT TS-Contador de Calorías: Creando el botón para llamar la acción de eliminar

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,7 +1,7 @@
 import {useMemo, Dispatch} from "react";
 import {Activity} from "../types";
 import {categories} from "../data/categories.ts";
-import {PencilSquareIcon} from "@heroicons/react/24/outline"
+import {PencilSquareIcon, XCircleIcon} from "@heroicons/react/24/outline"
 import {ActivityActions} from "../reducers/activity-reducer.ts";
 
 
@@ -12,9 +12,10 @@ type ActivityListProps = {
 
 function ActivityList({activities, dispatch}: ActivityListProps) {
 
-    const categoryName = useMemo(() => (category: Activity["category"]) => categories.map(
-            cat => cat.id === category ? cat.name : "")
-        , activities
+    const categoryName = useMemo(() =>
+            (category: Activity["category"]) => categories.map(
+                cat => cat.id === category ? cat.name : "")
+        , [activities]
     )
 
     return (
@@ -26,7 +27,8 @@ function ActivityList({activities, dispatch}: ActivityListProps) {
                 <div key={activity.id}
                      className="px-5 py-10 bg-white mt-5 flex justify-between">
                     <div className="space-y-2 relative">
-                        <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
+                        <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase 
+                        font-bold ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
                             {categoryName(+activity.category)}
                         </p>
                         <p className="text-2xl font-bold pt-5">{activity.name}</p>
@@ -44,6 +46,16 @@ function ActivityList({activities, dispatch}: ActivityListProps) {
                         >
                             <PencilSquareIcon
                                 className="h-8 w-8 text-gray-800"
+                            />
+                        </button>
+                        <button
+                            onClick={() => dispatch({
+                                type: "delete-activity",
+                                payload: {id: activity.id}
+                            })}
+                        >
+                            <XCircleIcon
+                                className="h-8 w-8 text-red-500"
                             />
                         </button>
                     </div>
