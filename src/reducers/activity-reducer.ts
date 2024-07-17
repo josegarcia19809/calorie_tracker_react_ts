ba21a903cf47942d10330b71bef24d3135003447
josegarcia19809 <josegarcia19809@gmail.com>
2024-07-16 22:23:30 -0600

    PROYECTO REACT TS-Contador de Calorías: Actualizando las actividades

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -22,9 +22,16 @@ export const activityReducer = (
 ) => {
     if (action.type === "save-activity") {
         // Este código maneja la lógica para actualizar el state
+        let updateActivities: Activity[] = []
+        if (state.activeId) {
+            updateActivities = state.activities.map(activity => activity.id === state.activeId ? action.payload.newActivity : activity)
+        } else {
+            updateActivities = [...state.activities, action.payload.newActivity]
+        }
         return {
             ...state,
-            activities: [...state.activities, action.payload.newActivity]
+            activities: updateActivities,
+            activeId: ""
         }
     }
     if (action.type === "set-activeId") {
